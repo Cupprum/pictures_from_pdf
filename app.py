43795f277820c9ebfcd249f52deb8ead30b3edff
Cupprum <branisa.samuel@windowslive.com>
2018-05-03 17:23:56 +0200

    added requirements

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from pdf2image import convert_from_path
-from PIL import Image
 import numpy as np
 import time
 
@@ -7,8 +6,11 @@ import time
 start_time = time.time()
 counter_of_done = 0
 
+input_name_of_pdf = input('nazov pdf priklad(zuzanka.pdf): ')
+input_name_of_pictures = input('nazov outputu priklad(maturita_1_): ')
+
 for alfa in range(1, 17):
-    im = convert_from_path('doc1.pdf')[alfa]
+    im = convert_from_path(input_name_of_pdf)[alfa]
 
     p = np.array(im)
     list1 = []
@@ -26,21 +28,17 @@ for alfa in range(1, 17):
             list1.append(line)
         line += 1
 
-    print(f"done pic analysis {(time.time() - start_time)}")
-
     final_list = list1[:]
 
     for x in range(0, len(list1) - 1):
         if list1[x] + 1 == list1[x + 1]:
             final_list.remove(list1[x + 1])
 
-    print(f"final_list {final_list}")
-
     for x in range(0, len(final_list) - 1):
         counter_of_done += 1
 
         zadanie = im.crop((133, final_list[x], 1520, final_list[x + 1]))
-        zadanie.save(f"foto{counter_of_done}.jpg")
+        zadanie.save(f"{input_name_of_pictures}{counter_of_done}.jpg")
 
     print(f"pic done in {(time.time() - start_time)}")
 print(f"--- {(time.time() - start_time)} seconds ---")
